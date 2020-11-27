29d776a03f72537764d3bb160a29ad53ecbff255
stawowcz <jakub.stawowczyk@nokia.com>
2020-11-27 22:59:58 +0100

    master1Commit

--- add_comment.py
+++ add_comment.py
@@ -29,3 +29,5 @@ def addComment(aCardId):
     comment = (json_response.get('display').get('entities').get('comment').get('text'))
     print ("This is added comment: " + comment)
     assert comment == vComment
+
+master 1 commit
